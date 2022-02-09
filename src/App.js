c8e61f9da22f5fccd16f76deacdd004f8fdc0474
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-09 15:41:27 -0800

    created App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   NavLink,
   Route,
   Redirect,
+  Link,
 } from 'react-router-dom';
 import AuthPage from './AuthPage';
 import DetailPage from './DetailPage';
@@ -17,12 +18,23 @@ import { logout } from './services/fetch-utils';
 
 export default function App() {
   // You'll need to track the user in state
-
+  const [currentUser, setCurrentUser] = useState(null);
   // add a useEffect to get the user and inject the user object into state on load
+  useEffect(() => {
+    async function fetch() {
+      const data = getUser();
+
+      setCurrentUser(data);
+    }
+
+    fetch();
+  }, []);
 
   async function handleLogout() {
     // call the logout function
+    await logout();
     // clear the user in state
+    currentUser('');
   }
 
   return (
@@ -30,20 +42,54 @@ export default function App() {
       <div className='App'>
         <header>
           {/* if there is a user in state, render out a link to the board games list, the create page, and add a button to let the user logout */}
+          {
+            currentUser &&
+              <ul>
+                <li>
+                  <Link to="/list">Board Games</Link>
+                </li>
+                <li>
+                  <Link to="/create">Create a Board Game</Link>
+                </li>
+                <li>
+                  <button onClick={handleLogout}>Logout</button>
+                </li>
+              </ul>
+          }
         </header>
         <main>
           <Switch>
             <Route exact path="/">
               {/* if there is a user, redirect to the board games list. Otherwise, render the auth page. Note that the AuthPage will need a function called setUser that can set the user state in App.js */}
+              {
+                currentUser
+                  ? <Redirect to="/list" />
+                  : <AuthPage setCurrentUser={setCurrentUser} />
+              }
             </Route>
             <Route exact path="/board-games">
               {/* if there is a user, render the board games list. Otherwise, redirect to the home route/auth page */}
+              {
+                currentUser
+                  ? <ListPage />
+                  : <Redirect to="/" />
+              }
             </Route>
             <Route exact path="/board-games/:id">
               {/* if there is a user, render the detail page. Otherwise, redirect to the home route/auth page */}
+              {
+                currentUser
+                  ? <DetailPage />
+                  : <Redirect to="/" />
+              }
             </Route>
             <Route exact path="/create">
               {/* if there is a user, render the create page. Otherwise, redirect to the home route/auth page */}
+              {
+                currentUser
+                  ? <CreatePage />
+                  : <Redirect to="/" />
+              }
             </Route>
           </Switch>
         </main>
