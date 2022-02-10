31a50e0edc768cf56acfb61170ee5625bde3072d
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-10 10:32:20 -0800

    fixed genre error on CreatePage

--- src/CreatePage.js
+++ src/CreatePage.js
@@ -49,7 +49,7 @@ export default function CreatePage() {
         <label>
             Genre
           {/* on change, set the genre in state */}
-          <select required>
+          <select required value={genre} onChange={e => setGenre(e.target.value)}>
             <option>Tile-laying</option>
             <option>Economic</option>
             <option>War</option>
