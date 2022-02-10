c1e159fdf5c0cc2a8a2e4d32b227271656664d60
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-10 09:59:35 -0800

    test #2

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Board Games/i);
+  const linkElement = screen.getByText(/players/i);
   expect(linkElement).toBeInTheDocument();
 });
